67203bce9e939a08b922386e149cf285f4980918
Thielly <thiellyvr@gmail.com>
2021-09-21 21:56:28 -0300

    Adicionando os arquivos necessarios do Projeto bancario

---
+++ DadosBancarios.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace Banco
+{
+    class DadosBancarios
+    {
+        public int NumeroConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+        
+        public DadosBancarios(int numeroconta, string nometitular)
+        {
+            NumeroConta = numeroconta;
+            Nome = nometitular;
+            Saldo = 0.00;
+        }
+
+        public DadosBancarios(int numeroconta, string nometitular, double saldo) : this(numeroconta, nometitular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo = (Saldo - valor) - 5;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {NumeroConta}, Titular: {Nome}, Saldo: R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Informe o número da conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+            Console.Write("Informe o titular da conta: ");
+            string nometitular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (S/N)? ");
+            char validardeposito = char.Parse(Console.ReadLine());
+
+            double depositoinicial = 0;
+
+            if (validardeposito == 'S' || validardeposito == 's')
+            {
+                Console.Write("Informe o valor do depósito inicial: ");
+                depositoinicial = double.Parse(Console.ReadLine());
+            }
+            else if (validardeposito == 'N' || validardeposito == 'n') 
+            {
+                Console.WriteLine();                
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida, tente novamente!");
+                return;
+            }
+
+            DadosBancarios conta = new DadosBancarios(numeroconta, nometitular, depositoinicial);
+            
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine($"{conta}");
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine());
+            conta.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine($"{conta}");
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine());
+            
+            if (saque == 0)
+            {
+                Console.WriteLine("Dados da conta: ");
+                Console.WriteLine($"{conta}");
+            }
+            else
+            {
+                conta.Saque(saque);
+                Console.WriteLine("Dados da conta atualizados: ");
+                Console.WriteLine($"{conta}");
+            }
+        }
+    }
+}
